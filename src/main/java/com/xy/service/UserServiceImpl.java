ec97882e9f2d158e0639b342b5a3d2b501f5f0d1
xiuyang <jlss2011@163.com>
2016-10-27 10:43:04 +0800

    mybatis config

--- src/main/java/com/xy/service/UserServiceImpl.java
+++ src/main/java/com/xy/service/UserServiceImpl.java
@@ -5,6 +5,7 @@ import com.xy.bean.User;
 import com.xy.bean.UserCreateForm;
 import com.xy.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -21,23 +22,23 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private UserMapper userMapper;
 
-    @Override
+    @Async
     public Optional<User> getUserById(long id) {
         //return Optional.ofNullable(new User(1L,"admin@163.com","$2a$10$ebyC4Z5WtCXXc.HGDc1Yoe6CLFzcntFmfse6/pTj7CeDY5I05w16C", Role.ADMIN));
         return Optional.ofNullable(userMapper.findById(id));
     }
 
-    @Override
+    @Async
     public Optional<User> getUserByEmail(String email) {
         return Optional.ofNullable(userMapper.findByEmail(email));
 }
 
-    @Override
+    @Async
     public Collection<User> getAllUsers() {
         return Arrays.asList(new User(1L,"admin@163.com","$2a$10$ebyC4Z5WtCXXc.HGDc1Yoe6CLFzcntFmfse6/pTj7CeDY5I05w16C", Role.ADMIN),new User(2L,"jlss2011@163.com","$2a$10$ebyC4Z5WtCXXc.HGDc1Yoe6CLFzcntFmfse6/pTj7CeDY5I05w16C", Role.USER));
     }
 
-    @Override
+    @Async
     public User create(UserCreateForm form) {
         User user = new User();
         user.setEmail(form.getEmail());
